00c37c158c36c19b540430e62c363e9edcfb27cb
Ranguasaman Salomon Joseph grosvener Rahankoly <6181901043@student.unpar.ac.id>
2022-10-26 17:44:32 +0700

    fragment buat temuan, sudah boleh pilih tanggal dan waktu

--- app/src/main/java/com/example/tubes01/FragmentPertemuan.java
+++ app/src/main/java/com/example/tubes01/FragmentPertemuan.java
@@ -1,17 +1,27 @@
 package com.example.tubes01;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.tubes01.databinding.FragmentHomeBinding;
 import com.example.tubes01.databinding.FragmentPertemuanBinding;
 
-public class FragmentPertemuan extends Fragment {
+import java.util.Calendar;
+import java.util.Locale;
+
+public class FragmentPertemuan extends Fragment implements DatePickerDialog.OnDateSetListener,View.OnClickListener {
     FragmentPertemuanBinding binding;
+    int hour = 0;
+    int minute = 0;
 
     public static FragmentPertemuan newInstance(String title){
         FragmentPertemuan fragment = new FragmentPertemuan();
@@ -25,8 +35,52 @@ public class FragmentPertemuan extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         this.binding = FragmentPertemuanBinding.inflate(inflater,container,false);
 
-
+        binding.ivCalender.setOnClickListener(this);
+        binding.ivWaktu.setOnClickListener(this);
 
         return binding.getRoot();
+    }
+
+    private void showDatePicker(){
+        DatePickerDialog dpd = new DatePickerDialog(
+                this.getContext(),
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        dpd.show();
+    }
+
+    private void showTimePicker(){
+        TimePickerDialog.OnTimeSetListener timeListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hourSelect, int minuteSelect) {
+                hour = hourSelect;
+                minute = minuteSelect;
+                binding.tvWaktuOut.setText(String.format(Locale.getDefault(),"%02d:%02d",hour,minute));
+            }
+        };
+
+        int style = AlertDialog.THEME_HOLO_DARK;
+
+        TimePickerDialog timePicker = new TimePickerDialog(this.getContext(),style, timeListener, hour,minute,true);
+
+        timePicker.setTitle("Pilih Waktu Temu");
+        timePicker.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+        binding.tvTanggalOut.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==binding.ivCalender){
+            showDatePicker();
+        }else if(view==binding.ivWaktu){
+            showTimePicker();
+        }
     }
 }
